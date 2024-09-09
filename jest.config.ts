a9acf0a852067af64c0b3a2ee4b1246ce56466b8
MikeLautensack <mikelautensack100@gmail.com>
2024-09-08 22:31:08 -0400

    fixed test script

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,22 @@
-module.exports = {
+export default {
+  preset: "ts-jest/presets/default-esm", // Use the ESM preset for ts-jest
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        useESM: true, // Enable ESM in ts-jest
+      },
+    ],
   },
   testEnvironment: "node",
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"], // Treat TypeScript files as ES modules
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore dist folder
+  moduleNameMapper: {
+    // Mock non-JS file imports if necessary
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
 };
